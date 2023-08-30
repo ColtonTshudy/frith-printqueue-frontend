62d1f96968ee17ac8f42b4bd55bc896bb7ec943d
ColtonTshudy <coltont@vt.edu>
2023-08-30 00:38:38 -0400

    adds all trainings in today and the next 2 days to an array

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState, useRef } from 'react'
 import CalendarDay from './calendar-day'
 import './css/calendar.css'
 
+const DEBUG_OFFSET = 0; //day offset for testing purposes
+
 const class_old = "course_109922";
 const class_old2 = "course_99575";
 const class_old3 = "course_121352";
 const class_ula = "course_162912";
 const class_production = "course_180929";
 
-const class_id = class_old2;
+const class_id = class_ula;
 
 const url1 = "http://localhost:3100/canvas-set"
 const url2 = "http://localhost:3100/canvas-get"
@@ -18,20 +20,20 @@ const Calendar = ({ className, data }) => {
     // if(data) {
     //     test = Object.keys(data).length ? 'green' : 'black'
     // }
-    const [date, setDate] = useState(new Date());
+    const [today, setDate] = useState(new Date());
     const [appointments, setAppointments] = useState({})
     const ref = useRef();
 
     useEffect(() => {
         setDate(new Date())
-        getAppointments(data, date);
+        getAppointments(data, today);
     }, [data])
 
     return (
         <div className={className} ref={ref} id="calendar-main">
-            <CalendarDay className="calendar-day blur" day={date.getDay()} />
-            <CalendarDay className="calendar-day calendar-sub blur" day={date.getDay() + 1} />
-            <CalendarDay className="calendar-day calendar-sub blur" day={date.getDay() + 2} />
+            <CalendarDay className="calendar-day blur" day={today.getDay()} />
+            <CalendarDay className="calendar-day calendar-sub blur" day={today.getDay() + 1} />
+            <CalendarDay className="calendar-day calendar-sub blur" day={today.getDay() + 2} />
         </div>
     )
 }
@@ -72,14 +74,60 @@ const getAppointments = (data, date) => {
 }
 
 //returns a dictionary of all meetings belonging to a day
-const sortByDays = (data, days, date) => {
+const sortByDays = (data, numDays, today) => {
+    //output structure:
+    /*
+    [
+        {//day 1 (today)
+            0 : {<details>}, //one of today's appointments
+            1 : ...
+        }
+        {//day 2 (tomorrow)
+            ...
+        },
+        ...
+    ]
+    */
+    const finalArray = Array.from(Array(numDays), () => new Array())
+
     const keys = Object.keys(data);
 
     keys.forEach((key, index) => {
-        console.log(data[key]);
+        // console.log(data[key]);
+        data[key].forEach((e) => {
+            console.log(e.start_at)
+            const test = new Date(e.start_at)
+            console.log(test.getTime())
+
+            //iterate through day offsets, 0 to <numDays>
+            for (const offset in [...Array(numDays).keys()]) {
+                if (isDate(test, today, parseInt(offset) + DEBUG_OFFSET)) {
+                    finalArray[offset].push(e)
+                    console.log(e)
+                }
+            }
+        })
     })
 
+    console.log(finalArray)
+
     return 0
+}
+
+/**
+ * Check if two dates are on the same day, plus a given offset
+ * @param {*} dateOther - date in question
+ * @param {*} dateBaseline - date to compare against
+ * @param {*} offsetDays - days of offset to apply to dateBaseline
+ * @returns true if dateOther is on dateBaseline + offsetDays, false otherwise
+ */
+const isDate = (dateOther, dateBaseline, offsetDays = 0) => {
+    const dateOffset = new Date(dateBaseline)
+    dateOffset.setDate(dateOffset.getDate() + offsetDays)
+
+    return dateOther.getDate() === dateOffset.getDate() &&
+        dateOther.getMonth() === dateOffset.getMonth() &&
+        dateOther.getYear() === dateOffset.getYear();
 }
 
 export default Calendar

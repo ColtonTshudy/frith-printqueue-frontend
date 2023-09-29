8bb22d037c8ffb86cf1366da4d254db6c1fcadec
ColtonTshudy <82547877+ColtonTshudy@users.noreply.github.com>
2023-09-29 10:42:36 -0400

    added reboot script

---
+++ reboot-no-wifi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Edit /etc/crontab using sudo and add the following line (replace yourname with your actual username):
+#*/30 * * * * sh /home/orangepi/frith-printqueue-frontend/reboot-no-wifi.sh
+
+TMP_FILE=/tmp/inet_up
+
+# Edit this function if you want to do something besides reboot
+no_inet_action() {
+    shutdown -r +1 'No internet.'
+}
+
+if ping -c5 google.com; then
+    echo 1 > $TMP_FILE
+else
+    [[ `cat $TMP_FILE` == 0 ]] && no_inet_action || echo 0 > $TMP_FILE
+fi
